21a42298462f2c2e135dd1778b27d5e99c2f4b5c
liulong <858243778@qq.com>
2022-04-05 23:23:17 +0800

    Update 197排列序号.py

--- lintcode/197排列序号.py
+++ lintcode/197排列序号.py
@@ -5,7 +5,7 @@ Class Solution:
     def Permutation_index(self, A):
         result = 1
         factor = 1
-        for i in range(len(A), -1, -1):
+        for i in range(len(A) - 1, -1, -1):
             rank = 0
             for j in range(i+1, len(A)):
                 if A[i] > A[j]:
